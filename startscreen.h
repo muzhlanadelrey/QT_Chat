dc303a8d7c8080eb1ac5dda20a4a7c9d7edc77b4
Anastasia <126528333+muzhlanadelrey@users.noreply.github.com>
2023-10-31 23:03:09 +0300

    Add files via upload

---
+++ startscreen.h
@@ -0,0 +1,40 @@
+#ifndef STARTSCREEN_H
+#define STARTSCREEN_H
+
+#include <QDialog>
+#include <memory>
+#include "Database.h"
+
+namespace Ui {
+class StartScreen;
+}
+
+class StartScreen : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit StartScreen(std::shared_ptr<Database> dbPtr = nullptr, QWidget *parent = nullptr);
+    ~StartScreen();
+
+    void setLoginForm();
+    void setRegistrationForm();
+
+    int userId() const;
+
+    QString userName() const;
+
+    std::shared_ptr<Database> getDatabase() const;
+
+public slots:
+    void onLoggedIn(uint userId, QString userName);
+    void onRejectedRequested();
+
+private:
+    Ui::StartScreen *ui;
+    int m_userId;
+    QString m_userName;
+    std::shared_ptr<Database> m_dbPtr;
+};
+
+#endif // STARTSCREEN_H
